49e561d18b13868d71e4beb04bd94ceb4346e39e
Nick Zhihovich <nickzhihovich@gmail.com>
2019-10-05 12:26:15 +0300

    Add seed categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,23 @@
-User.create(email: 'nickzhihovich@gmail.com', password: '80657214')
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation
+# then, whenever you need to clean the DB
+DatabaseCleaner.clean
+
+user = User.create!(email: 'nickzhihovich@gmail.com', password: '80657214')
+budget = Budget.create!(name: 'Home', user: user)
+
+category_group_learning = CategoryGroup.create!(title: 'Learning', budget: budget)
+category_group_fun = CategoryGroup.create!(title: 'Fun', budget: budget)
+category_group_general = CategoryGroup.create!(title: 'General expenses', budget: budget)
+category_group_software = CategoryGroup.create!(title: 'Software', budget: budget)
+
+category1 = Category.create!(title: 'English', category_group: category_group_learning)
+category2 = Category.create!(title: 'Udemy courses', category_group: category_group_learning)
+category3 = Category.create!(title: 'Bars', category_group: category_group_fun)
+category4 = Category.create!(title: 'Cafe', category_group: category_group_fun)
+category5 = Category.create!(title: 'Bowling', category_group: category_group_fun)
+category6 = Category.create!(title: 'Home payments', category_group: category_group_general)
+category7 = Category.create!(title: 'Internet', category_group: category_group_general)
+category8 = Category.create!(title: 'Mobile', category_group: category_group_general)
+category9 = Category.create!(title: 'Petrol', category_group: category_group_general)
+category10 = Category.create!(title: 'Rubymine', category_group: category_group_software)
