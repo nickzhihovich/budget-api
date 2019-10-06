2220f225f3e19b987815902df971e9c9bec47365
Nick Zhihovich <nickzhihovich@gmail.com>
2019-10-06 23:43:58 +0300

    Add test for budget creation which checks default categories creator

--- spec/services/budgets/creator_spec.rb
+++ spec/services/budgets/creator_spec.rb
@@ -31,4 +31,25 @@ describe Budgets::Creator do
       end.to change(Budget, :count).by(0)
     end
   end
+
+  context 'default categories creation' do
+    let(:expected_category_groups) do
+      path_to_file = Rails.root.join('lib', 'settings', 'default_categories_groups.json')
+      JSON.parse(File.read(path_to_file))
+    end
+
+    let(:count_of_categories) do
+      expected_category_groups.map{|item| item['categories']}.flatten.count
+    end
+    
+    let(:budget) { described_class.new(user: user, params: {name: budget_name }).call }
+
+    it 'count of category groups' do
+      expect(budget.category_groups.count).to eq(expected_category_groups.count)
+    end
+
+    it 'count of categories' do
+      expect(budget.category_groups.map(&:categories).flatten.count).to eq(count_of_categories)
+    end
+  end
 end
