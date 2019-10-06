ebb7fcbb533b1a08e5a3e1cec3373f8a9621f4e5
Nick Zhihovich <nickzhihovich@gmail.com>
2019-10-06 20:41:31 +0300

    Add scopes for accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,7 @@ class Account < ApplicationRecord
   belongs_to :budget
 
   validates :amount_cents, :title, :kind, :budget, presence: true
+
+  scope :deposits, -> { where(kind: 'deposit') }
+  scope :credits, -> { where(kind: 'credit') }
 end
