b142e5512032b822ac4050f108797181b0a58a92
Nick Zhihovich <nickzhihovich@gmail.com>
2019-10-09 18:54:46 +0300

    Add account entity

---
+++ app/api/v1/entities/account.rb
@@ -0,0 +1,13 @@
+module V1
+  module Entities
+    class Account
+      expose :title
+      expose :amount_cents
+      expose :amount_currency
+      expose :kind
+      expose :budget_id
+      expose :created_at
+      expose :updated_at
+    end
+  end
+end
