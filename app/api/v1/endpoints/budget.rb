eb97bbb6d50c6cb4d8a6b9f56eb6bf312bfea37f
Nick Zhihovich <nickzhihovich@gmail.com>
2019-10-09 19:09:00 +0300

    Add routes to get budget and list of budgets

--- app/api/v1/endpoints/budget.rb
+++ app/api/v1/endpoints/budget.rb
@@ -1,17 +1,24 @@
 module V1
   module Endpoints
     class Budget < Base
-      before do
-        doorkeeper_authorize!
-      end
+      # before do
+      #   doorkeeper_authorize!
+      # end
 
+      desc 'Return a list of budgets.'
       namespace 'budgets' do
         get do
-          current_user.budget
+          current_user.budgets
         end
 
-        get :id do
-          user.budgets.find(params[:id])
+        desc 'Return a budget.'
+        params do
+          requires :id, type: Integer, desc: 'Budget id.'
+        end
+        route_param :id do
+          get do
+            current_user.budgets.find(params[:id])
+          end
         end
       end
     end
